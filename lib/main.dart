8ccaa37b18b111235053741f7d2a65ce400981d5
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-07-28 00:39:55 +0530

    Scaffold and AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Text('hey luigi')
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('hello, luigi'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: null,
+      child: Text('click'),
+    ),
+  ),
 ));
