ef0aa46adf41350c671a8972cf91eb623d858cc6
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-07-28 00:43:56 +0530

    Colours and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,24 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600]
     ),
     body: Center(
-      child: Text('hello, luigi'),
+      child: Text(
+        'hello, Luigi',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.red[600],
       onPressed: null,
       child: Text('click'),
     ),
