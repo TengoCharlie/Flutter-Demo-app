740e5268a5e4c96591995e713390d3791f8a473b
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-08-01 22:48:28 +0530

    Expendad n flex

--- lib/main.dart
+++ lib/main.dart
@@ -3,48 +3,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
 
-
 class Home extends StatelessWidget {
   // const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(//Wrapper to make layout
+    return Scaffold(
+      //Wrapper to make layout
       appBar: AppBar(
         title: Text("My App"),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Row(
         children: [
-          Row(
-            children: [
-              Text("Hello"),
-              Text("World")
-            ],
+          Expanded(
+            child: Image.asset('./assets/one.jpg'),
+            flex: 3,
           ),
-          Container(
-            color: Colors.cyan,
-            padding: EdgeInsets.all(20),
-            child: Text('One'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
           ),
-          Container(
-            color: Colors.red,
-            padding: EdgeInsets.all(30),
-            child: Text('Two'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: Text('2'),
+            ),
           ),
-          Container(
-            color: Colors.yellow,
-            padding: EdgeInsets.all(40),
-            child: Text('Three'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('3'),
+            ),
           ),
-
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Text("Click"),
         backgroundColor: Colors.red[600],
       ),
